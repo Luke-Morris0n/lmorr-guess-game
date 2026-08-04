6c8a2e55e5d7dc134ca105d10cdc743735678f8b
Luke Morrison <l.morrison@corecomtechacademy.co.uk>
2026-08-04 16:10:01 +0100

    final version

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -1,25 +1,30 @@
+import com.sun.source.tree.WhileLoopTree;
+
 import java.util.Scanner;
 
 public class Game {
 
     void main() {
 
+        int countdown = 4;
+
         int randomNum = (int) (Math.random() * 51);
         System.out.println(randomNum);
 
-        Scanner guesses = new Scanner(System.in);
-        System.out.println("Enter First Guess");
-        int guessOne = guesses.nextInt();
-        if (guessOne == randomNum){
-            System.out.println("Victory!");
-        } else if (guessOne > randomNum){
-            System.out.println("Too High!");
-            System.out.println("Enter Second Guess");
-        } else {
-            System.out.println("Too Low!");
-            System.out.println("Enter Second Guess");
+        while (countdown > 0) {
+            Scanner guesses = new Scanner(System.in);
+            System.out.println("Enter Guess");
+            int guessedNum = guesses.nextInt();
 
+            if (guessedNum > randomNum) {
+                System.out.println("Too High!");
+            } else if (guessedNum < randomNum) {
+                System.out.println("Too Low!");
+            } else {
+                System.out.println("Victory!");
+                countdown = 0;
+            }
+            countdown--;
         }
-
     }
 }
