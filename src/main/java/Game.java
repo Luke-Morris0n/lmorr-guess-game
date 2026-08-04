e21f240747091806aa6720e2ed7e9d1c83c7f8a6
Luke Morrison <l.morrison@corecomtechacademy.co.uk>
2026-08-04 15:20:49 +0100

    please_work

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -1,2 +1,25 @@
+import java.util.Scanner;
+
 public class Game {
+
+    void main() {
+
+        int randomNum = (int) (Math.random() * 51);
+        System.out.println(randomNum);
+
+        Scanner guesses = new Scanner(System.in);
+        System.out.println("Enter First Guess");
+        int guessOne = guesses.nextInt();
+        if (guessOne == randomNum){
+            System.out.println("Victory!");
+        } else if (guessOne > randomNum){
+            System.out.println("Too High!");
+            System.out.println("Enter Second Guess");
+        } else {
+            System.out.println("Too Low!");
+            System.out.println("Enter Second Guess");
+
+        }
+
+    }
 }
